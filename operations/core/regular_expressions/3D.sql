e03779ef79a0cf7c120f3af960930a4d8daaf108
Sam Chau <chaucted@gmail.com>
2025-11-03 05:02:38 +1030

    full db test

--- operations/core/regular_expressions/3D.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO regular_expressions (name, pattern, regex101_id, description)
-VALUES (
-    '3D',
-    '(?<=\b[12]\d{3}\b).*\b((bluray|bd)?3d|sbs|half[ .-]ou|half[ .-]sbs)\b',
-    'wSkb7u/1',
-    'Matches terms related to 3D video formats:\n- `bluray3d` or `bd3d` (optional `bluray` or `bd` followed by `3d`).\n- `sbs` (side-by-side).\n- `half ou` or `half sbs` with space (` `), dot (`.`), or hyphen (`-`) as separators.'
-);
